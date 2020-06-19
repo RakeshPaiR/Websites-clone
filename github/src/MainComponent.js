8b0d952215241aa2b94911f379db6e632b48ced5
RakeshPaiR <rakeshpair1998@gmail.com>
2020-06-19 22:57:17 +0530

    RP: initialised folders

---
+++ github/src/MainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Landing from './pages/Landing/Landing'
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Landing />
+            </div>
+        )
+    }
+}
+
+export default Main;
