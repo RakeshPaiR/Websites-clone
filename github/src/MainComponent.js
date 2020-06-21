9334d6ac775f90679eaef00bd3abd6724a06d992
Augustine <augustinetharakan12@gmail.com>
2020-06-21 22:07:52 +0530

    AT: Navbar initial structure

--- github/src/MainComponent.js
+++ github/src/MainComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Landing from './pages/Landing/Landing'
+import Navbar from './components/Navbar/Navbar'
 
 class Main extends Component {
     render() {
         return (
             <div>
+                <Navbar />
                 <Landing />
             </div>
         )
