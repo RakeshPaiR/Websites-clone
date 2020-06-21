a533c47ac1c28611672541bca338fdf6e7ef9472
RakeshPaiR <rakeshpair1998@gmail.com>
2020-06-21 23:32:15 +0530

    RP: completed Getstarted component

--- github/src/pages/Landing/Landing.js
+++ github/src/pages/Landing/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
+import Getstarted from '../../components/Getstarted/Getstarted';
 
 function Landing() {
     return(
         <>
             <Hero />
+            <Getstarted />
         </>
     );
 }
